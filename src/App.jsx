e7cd164bd04b7ced4b6e514025a1d36e1922470e
فهد وسام عبدالرضا شندي <fhdwsambdalrdashndy@alMacBook-Air-alkhas-bfhd.local>
2025-08-12 15:31:50 -0700

    Fix import paths with extensions

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Products from './pages/Products.jsx'
-import SubSections from './pages/SubSections.jsx'
 import Suppliers from './pages/Suppliers.jsx'
 import Categories from './pages/Categories.jsx'
 import Navbar from './components/Navbar/Navbar.jsx'
@@ -16,12 +15,10 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<Products />} />
-        <Route path='/subSections' element={<SubSections />} />
         <Route path='/suppliers' element={<Suppliers />} />
         <Route path='/categories' element={<Categories />} />
         <Route path='/login' element={<Login />} />
         <Route path='/offers' element={<Offers />} />
-        <Route path='*' element={<div>404 Not Found</div>} />
       </Routes>
     </div>
   )
