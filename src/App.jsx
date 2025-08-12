8d8abfb0009f4da5beb8346e726ac7c2b7c2b7ab
فهد وسام عبدالرضا شندي <fhdwsambdalrdashndy@alMacBook-Air-alkhas-bfhd.local>
2025-08-12 16:51:20 -0700

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Products from './pages/Products'
 import Categories from './pages/Categories'
 import Navbar from './components/Navbar/Navbar'
 import Login from './pages/Login'
-import Offers from './pages/offers'
-import suppliers from './pages/suppliers'
+import Offers from './pages/Offers'
+import Suppliers from './pages/Suppliers'
 import SubSections from './pages/SubSections'
 
 const App = () => {
@@ -19,13 +19,11 @@ const App = () => {
         <Route path='/categories' element={<Categories />} />
         <Route path='/login' element={<Login />} />
         <Route path='/offers' element={<Offers />} />
-        <Route path='/suppliers' element={<suppliers />} />
+        <Route path='/suppliers' element={<Suppliers />} />
         
-        {/* Catch-all route for undefined paths */}
-        {/* This will render a simple message for any unmatched route */}
+
         <Route path='/not-found' element={<h1>Page Not Found</h1>} />
         <Route path='/error' element={<h1>Something went wrong</h1>} />
-        {/* You can also use a wildcard route to catch all other paths */}
         <Route path='*' element={<h1>mwdwdjwedjwd</h1>} />
         <Route path='/subsections' element={<SubSections />} />
       </Routes>
